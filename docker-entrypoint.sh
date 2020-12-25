4a204e3a4da36ee1b37bf41068a1bdbdc222db95
Julian Merkle <juli.merkle@gmail.com>
2020-12-25 22:05:25 +0100

    Add Docker setup
    - BASE_HREF support
    - docker-compose uses docker image from jvmerkle/ts3-ranksystem
    - Reduce unnecessary docker layers
    - Add docker badges to the README.md
    - Apache2 and PHP hardening
    - Set cap_net_bind_service on apache2
    - "Proper"/fast container shutdown

---
+++ docker-entrypoint.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+# Copyright (C) 2020 Julian Merkle <juli.merkle@gmail.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+set -e
+
+if [ ! "$(whoami)" = "www-data" ]; then
+  echo Script must be run as user www-data
+  exit 1
+fi
+
+# Remove the old instance (if existing)
+rm -rf "/var/www/html/"
+
+# Setup base href
+BASE_HREF="${BASE_HREF}/"
+RS_ROOT="/var/www/html/${BASE_HREF}"
+
+# Copy the RS to the base href
+mkdir -p "$RS_ROOT"
+cp -r "/var/www/ranksystem/." "$RS_ROOT"
+
+# Copy repository tsicons to the volume
+cp -r "${RS_ROOT}tsicons/." /var/www/tsicons/
+
+# Delete the repository tsicons
+rm -rf "${RS_ROOT}tsicons"
+
+# Create the link for the tsicons
+ln -s /var/www/tsicons/ "${RS_ROOT}tsicons"
+
+cat >"${RS_ROOT}other/dbconfig.php" <<EOF
+<?php
+\$db['type']="$DB_TYPE";
+\$db['host']="$DB_HOST";
+\$db['user']="$DB_USERNAME";
+\$db['pass']="$DB_PASSWORD";
+\$db['dbname']="$DB_NAME";
+?>
+EOF
+
+worker() {
+  cd "${RS_ROOT}"
+  while true; do
+    php worker.php check
+    sleep $(( 10 * 60 )) # Every 10 minutes
+  done
+}
+
+if [ -z "$INSTALL_MODE" ] || [ "$INSTALL_MODE" = "0" ]; then
+  rm -rf "${RS_ROOT}install.php"
+  worker &
+else
+  rm -rf "${RS_ROOT}stats" \
+         "${RS_ROOT}webinterface" \
+         "${RS_ROOT}index.php" \
+         "${RS_ROOT}worker.php"
+fi
+
+apache2-foreground
