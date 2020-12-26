26e2a3816e0454d16a94665a211faa5fa8ff957b
Julian Merkle <juli.merkle@gmail.com>
2020-12-26 09:34:10 +0100

    Adjust aesthetics
    - Move ranksystem information to the footer
    - Update ranksystem information page
    - Change website title
    - Remove site favicon
    
    (cherry picked from commit 45513c2a30ecc843b584de915976154866bd2e2a)

--- stats/_footer.php
+++ stats/_footer.php
@@ -1,15 +1,18 @@
 <?PHP
+echo '<footer>';
+echo '<a href="info.php">' . $lang['stnv0030'] . '</a>';
+
 if ($cfg['stats_imprint_switch'] == "1") {
 	if($cfg['stats_imprint_address_url'] != NULL)  {
-		echo '<footer><a href="',$cfg['stats_imprint_address_url'],'" target="_blank">',$lang['imprint'],'</a>';
+		echo '&nbsp;•&nbsp;<a href="',$cfg['stats_imprint_address_url'],'" target="_blank">',$lang['imprint'],'</a>';
 	} else {
-		echo '<footer><a href="imprint.php">',$lang['imprint'],'</a>';
+		echo '&nbsp;•&nbsp;<a href="imprint.php">',$lang['imprint'],'</a>';
 	}
 	if($cfg['stats_imprint_privacypolicy_url'] != NULL)  {
 		echo '&nbsp;•&nbsp;<a href="',$cfg['stats_imprint_privacypolicy_url'],'" target="_blank">',$lang['privacy'],'</a>';
 	} elseif($cfg['stats_imprint_privacypolicy'] != NULL)  {
 		echo '&nbsp;•&nbsp;<a href="privacy_policy.php">',$lang['privacy'],'</a>';
 	}
-	echo '</footer>';
 }
+echo '</footer>';
 ?>
