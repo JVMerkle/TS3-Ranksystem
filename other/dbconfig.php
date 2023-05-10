62afeeedc2a354fdaaac1a8be8fddbf8347961c2
LickAbrick <g.wikkerink@krokbv.com>
2023-05-10 22:25:06 +0200

    Use empty default values for dbconfig.php

--- other/dbconfig.php
+++ other/dbconfig.php
@@ -1,7 +1,7 @@
 <?php
-$db['type']="type";
-$db['host']="hostname";
-$db['user']="dbuser";
-$db['pass']="dbpass";
-$db['dbname']="ts3_ranksystem";
+$db['type']=""; // Database type
+$db['host']=""; // IP address or hostname
+$db['user']=""; // Database user name
+$db['pass']=""; // Database password
+$db['dbname']=""; // Database name
 ?>
